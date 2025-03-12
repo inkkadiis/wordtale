3fc15fadc7afd0627ff5eb716fba991a6a67a2a9
Elias <insj1092@gmail.com>
2025-03-12 04:15:11 -0700

    0312 fisrt

---
+++ src/mainPage.jsx
@@ -0,0 +1,95 @@
+import bookMark from './assets/IcnBookmark.svg'
+
+function Pages (){
+
+    return (
+    <div className="w-screen flex justify-center bg-[#FDF9F0]">    
+        <div className="w-md h-[932px] relative bg-[#fdf8f0] overflow-hidden">
+        <div className="w-96 h-[641px] left-[800px] top-[721px] absolute origin-top-left -rotate-180 bg-white rounded-tl-3xl rounded-bl-3xl shadow-[1px_5px_11px_0px_rgba(147,145,133,0.10)] shadow-[5px_19px_20px_0px_rgba(147,145,133,0.09)] shadow-[12px_44px_27px_0px_rgba(147,145,133,0.05)] shadow-[22px_78px_32px_0px_rgba(147,145,133,0.01)] shadow-[34px_121px_35px_0px_rgba(147,145,133,0.00)] border border-[#f4f1eb]" />
+        <div className="w-96 h-[625px] left-[792px] top-[713px] absolute origin-top-left -rotate-180 rounded-tl-[19px] rounded-bl-[19px] border-l-4 border-t-4 border-b-4 border-[#f4f1eb]" />
+        <div className="w-96 left-[24px] top-[80px] absolute inline-flex flex-col justify-start items-center gap-6">
+            <div className="self-stretch h-[641px] relative rounded-tl-3xl rounded-bl-3xl ">
+            <div className="w-96 h-[641px] left-0 top-0 absolute bg-white rounded-tl-3xl rounded-bl-3xl border border-[#f4f1eb] "  />
+            <div className="w-96 h-[625px] left-[8px] top-[8px] absolute rounded-tl-[19px] rounded-bl-[19px] border-l-4 border-t-4 border-b-4 border-[#f4f1eb]" />
+            <div className="w-80 left-[36px] top-[120px] absolute inline-flex flex-col justify-start items-start gap-9">
+                <div className="w-32 h-14 relative">
+                <div className="left-0 top-0 absolute justify-start text-black text-5xl font-bold font-['Outfit']">Left</div>
+                <div className="left-[84px] top-[22px] absolute justify-start text-[#a03c06] text-xl font-medium font-['EB_Garamond'] tracking-tight">/left/</div>
+                </div>
+                <div className="self-stretch flex flex-col justify-start items-start gap-8">
+                <div className="self-stretch inline-flex justify-start items-center gap-6">
+                    <div className="flex-1 inline-flex flex-col justify-start items-start gap-1">
+                    <div className="inline-flex justify-start items-start gap-1">
+                        <div className="justify-start text-black text-base font-normal font-['Outfit']">1. </div>
+                        <div className="justify-start text-[#ff8340] text-base font-semibold font-['Outfit'] uppercase">ADJECTIVE</div>
+                    </div>
+                    <div className="self-stretch justify-start text-black text-base font-normal font-['EB_Garamond'] leading-snug">on, toward, or relating to the side of a human body or of a thing that is to the west when the person or thing is facing north.</div>
+                    </div>
+                    <div className="w-6 h-6 relative overflow-hidden">
+                    <div className="w-5 h-5 left-[2px] top-[2px] absolute outline-2 outline-offset-[-1px] outline-[#b8af9f]" />
+                    </div>
+                </div>
+                <div className="self-stretch inline-flex justify-start items-center gap-6">
+                    <div className="flex-1 inline-flex flex-col justify-start items-start gap-1">
+                    <div className="inline-flex justify-start items-start gap-1">
+                        <div className="justify-start text-black text-base font-normal font-['Outfit']">2. </div>
+                        <div className="justify-start text-[#ff8340] text-base font-semibold font-['Outfit'] uppercase">ADJECTIVE</div>
+                    </div>
+                    <div className="self-stretch justify-start text-black text-base font-normal font-['EB_Garamond'] leading-snug">relating to or denoting a person or group favoring left-wing views; progressive.</div>
+                    </div>
+                    <div className="w-6 h-6 relative overflow-hidden">
+                    <div className="w-5 h-5 left-[2px] top-[2px] absolute outline-2 outline-offset-[-1px] outline-[#b8af9f]" />
+                    </div>
+                </div>
+                <div className="self-stretch inline-flex justify-start items-center gap-6">
+                    <div className="flex-1 inline-flex flex-col justify-start items-start gap-1">
+                    <div className="inline-flex justify-start items-start gap-1">
+                        <div className="justify-start text-black text-base font-normal font-['Outfit']">3. </div>
+                        <div className="justify-start text-[#ff8340] text-base font-semibold font-['Outfit'] uppercase">Noun</div>
+                    </div>
+                    <div className="self-stretch justify-start text-black text-base font-normal font-['EB_Garamond'] leading-snug">the left-hand part, side, or direction.</div>
+                    </div>
+                    <div className="w-6 h-6 relative overflow-hidden">
+                    <div className="w-5 h-5 left-[2px] top-[2px] absolute outline-2 outline-offset-[-1px] outline-[#b8af9f]" />
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div data-toggle="Saved" className="bg-transparent w-14 h-28 left-[286px] top-[-4px] absolute shadow-[0px_5px_11px_0px_rgba(147,145,133,0.10)] shadow-[0px_10px_20px_0px_rgba(147,145,133,0.09)] shadow-[0px_10px_10px_0px_rgba(147,145,133,0.02)] shadow-[0px_10px_10px_0px_rgba(147,145,133,0.01)] shadow-[0px_10px_20px_0px_rgba(147,145,133,0.00)]">
+                
+                <div  className= "w-14 h-28 bg-transparent"><img class="bookMark" src={bookMark}></img></div>
+            </div>
+            </div>
+            <div className="w-8 inline-flex justify-between items-center">
+            <div className="w-2 h-2 bg-[#ff8340] rounded-full" />
+            <div className="w-2 h-2 bg-[#d9d9d9] rounded-full" />
+            </div>
+            <div className="h-16 px-8 py-4 bg-white rounded-[40px] shadow-[-1px_5px_11px_0px_rgba(147,145,133,0.10)] shadow-[-5px_19px_20px_0px_rgba(147,145,133,0.09)] shadow-[-12px_44px_27px_0px_rgba(147,145,133,0.05)] shadow-[-22px_78px_32px_0px_rgba(147,145,133,0.01)] shadow-[-34px_121px_35px_0px_rgba(147,145,133,0.00)] shadow-[0px_2px_12px_0px_rgba(204,197,194,1.00)] outline-1 outline-offset-[-1px] outline-[#f4f1eb] inline-flex justify-start items-center gap-6">
+            <div className="w-9 h-9 relative">
+                <div className="w-9 h-9 left-0 top-0 absolute overflow-hidden">
+                <div className="w-3 h-6 left-[12px] top-[6px] absolute outline-[3px] outline-offset-[-1.50px] outline-[#b8af9f]" />
+                </div>
+            </div>
+            <div className="w-9 h-9 relative">
+                <div className="w-9 h-9 left-0 top-0 absolute overflow-hidden">
+                <div className="w-7 h-7 left-[4.50px] top-[3.40px] absolute outline-[3px] outline-offset-[-1.50px] outline-[#ff8340]" />
+                </div>
+            </div>
+            <div className="w-9 h-9 relative">
+                <div className="w-9 h-9 left-0 top-0 absolute overflow-hidden">
+                <div className="w-7 h-7 left-[4.50px] top-[4.50px] absolute outline-[3px] outline-offset-[-1.50px] outline-[#b8af9f]" />
+                </div>
+            </div>
+            <div className="w-9 h-9 relative">
+                <div className="w-9 h-9 left-0 top-0 absolute overflow-hidden">
+                <div className="w-7 h-6 left-[3px] top-[6px] absolute outline-[3px] outline-offset-[-1.50px] outline-[#b8af9f]" />
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    );
+}
+
+export default Pages
