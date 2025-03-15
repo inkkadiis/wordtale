a3e3010c1d01314c5658072cdd0da196b2539330
Elias <insj1092@gmail.com>
2025-03-15 00:58:46 -0700

    update: responsive

--- src/WordCard.jsx
+++ src/WordCard.jsx
@@ -4,9 +4,10 @@ import Icnbookmark from "./components/Icnbookmark";
 function WordCard() {
   return (
     <div className="Wordshortdef w-full max-w-md h-auto bg-m p-6 md:p-6 lg:p-8">
-      <div className="Contetnword w-full md:auto mx-auto relative flex flex-col justify-start items-center gap-6">
-        <div className="Wordbook w-full h-auto bg-white rounded-tl-3xl rounded-bl-3xl shadow-xl border border-borderColor p-9 ">
-          <div className="Wordcontents w-full md:auto mx-auto flex flex-col gap-9">
+      <div className="Contetnword w-full mx-auto relative flex flex-col justify-start items-center gap-6">
+        <div className="Wordbook w-full h-auto bg-white rounded-tl-3xl rounded-bl-3xl shadow-xl  p-9">
+          <div className="Wordcontents w-full mx-auto flex flex-col gap-9">
+            {/* Word + Pronunciation */}
             <div className="Wordwithpronoun w-full flex items-baseline md:flex-row gap-2">
               <h1 className="text-black text-4xl md:text-5xl font-bold">
                 Left
@@ -17,6 +18,8 @@ function WordCard() {
                 </div>
               </div>
             </div>
+
+            {/* Definitions */}
             <div className="Deflist flex flex-col gap-8">
               {[
                 {
@@ -39,7 +42,7 @@ function WordCard() {
               ].map((item, index) => (
                 <div
                   key={index}
-                  className="Deflistcontent flex flex-h md:flex-row items-center gap-6"
+                  className="Deflistcontent flex flex-col md:flex-row items-center gap-6"
                 >
                   <div className="flex-1 flex flex-col gap-1">
                     <div className="flex items-center gap-1">
@@ -59,8 +62,11 @@ function WordCard() {
               ))}
             </div>
 
-            <div>{/**viwe all div*/}</div>
+            {/* Placeholder for Future Content */}
+            <div>{/** View all div */}</div>
           </div>
+
+          {/* Keeping `Icnbookmark` as is, since it works now */}
           <Icnbookmark />
         </div>
       </div>
