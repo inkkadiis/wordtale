4d448f270fe1037a9a15146b147315b06eec19f7
henaring <henaring.100@gmail.com>
2025-03-15 11:34:19 +0900

    update: icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,15 @@
+import WandIcon from "./WandIcon";
+
 export default function SearchBar() {
   return (
     <div class="max-w-md rounded-full p-2 bg-gradient-to-b from-point to-yellow">
       <div class="rounded-full p-6 bg-white flex justify-between">
-        <input type="text" className="" placeholder="test"></input>
-        <span>icon</span>
+        <input
+          type="text"
+          className="outline-none focus:ring-0 focus:border-transparent"
+          placeholder="test"
+        ></input>
+        <WandIcon />
       </div>
     </div>
   );
